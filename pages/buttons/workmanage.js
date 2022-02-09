d5129dad14b0496e9f24abdc77e349079d360184
cyrusae <86699936+cyrusae@users.noreply.github.com>
2022-02-09 13:10:04 -0800

    WorkHide component for taking user entry
    Adds ability to enter URL or work ID.
    
    Needs: getting it to talk to the rest of the app.

--- pages/buttons/workmanage.js
+++ pages/buttons/workmanage.js
@@ -1,12 +1,36 @@
 import React, { Component } from 'react'
+import { Button } from 'react-bootstrap';
 
-class WorkHide extends Component {
+class WorkHide extends React.Component {
  
  constructor(props) {
   super(props);
+  this.state = {fic: ''};
 
+  this.handleChange = this.handleChange.bind(this);
+  this.handleSubmit = this.handleSubmit.bind(this);
+ }
+
+ handleChange(e) {
+  this.setState({fic: e.target.value})
+ }
+
+ handleSubmit(e) {
+  console.log(this.state.fic);
+  e.preventDefault();
+ }
+
+ render() {
+  const fic = this.state.fic;
+  return (
+   <form onSubmit={this.handleSubmit}>
+    <label>URL or Work ID:<br/>
+    <input type='text' value={fic} onChange={this.handleChange} /></label>
+   </form>
+  )
  }
 }
+
 
 export function WorkInfo() {
  return (
@@ -15,3 +39,5 @@ export function WorkInfo() {
   </>
  )
 }
+
+export default WorkHide 
